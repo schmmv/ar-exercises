2622b45e46f02fd0da1477668a9fb3e661f3c708
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-03 18:36:01 +0000

    Calculate total and average revenue, and how many stores make M or more

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,16 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Calculations
+# Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
+
+puts "Total revenue: #{Store.sum(:annual_revenue)}"
+puts "Average revenue: #{Store.average(:annual_revenue)}"
+@money_raking_stores = Store.where('annual_revenue >= ?', 1000000)
+
+puts "Stores generating $1M or more in annual sales: #{@money_raking_stores.size}"
+# for store in @money_raking_stores do
+#   puts store.name
+# end
