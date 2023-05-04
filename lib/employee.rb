911cef7ac6c3289a3a53e38374d60783c69c02a1
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-04 21:53:11 +0000

    Add model validation

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  #presence validation for FK 
+  validates :first_name, :last_name, :store_id, presence: true
+  # validates :hourly_rate, numericality: { only_integer: true }
+  validates :hourly_rate, numericality: { only_integer: true, in: 40..200 }
+
 end
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validate :carry_mens_or_womens_apparel, on: :create
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  private 
+
+  def carry_mens_or_womens_apparel
+    errors.add(:carry_mens_or_womens_apparel, "Store must carry at least one type of apparel") unless mens_apparel || womens_apparel
+  end
+  
 end
